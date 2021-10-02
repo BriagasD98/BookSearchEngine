f89c2977535b003df970f2acab046040fcda8b6a
BriagasD98 <briagasdavid@yahoo.com>
2021-10-02 18:06:01 -0500

    queries.js added

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+// "me" query which sets up Apollo Server
+export const GET_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
