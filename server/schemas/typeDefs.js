1633bb78375df0c89a2594705a928ff6b6796d22
BriagasD98 <briagasdavid@yahoo.com>
2021-10-03 10:53:22 -0500

    graphql is working now, issues with savedBooks being undefined

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,7 +7,7 @@ const typeDefs = gql`
 
     type User {
         _id: ID
-        username: String
+        username: String!
         email: String
         bookCount: Int
         savedBooks: [Book]
@@ -35,7 +35,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(book: SavedBookInput): User
+        saveBook(book: SavedBookInput!): User
         removeBook(bookId: String!): User
     }
 
